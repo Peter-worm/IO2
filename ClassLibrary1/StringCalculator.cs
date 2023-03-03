084ef927c43d9b85a8a183dd29e43a2c03136605
Peter-worm <1234ozmo@wp.pl>
2023-03-03 09:52:43 +0100

    test2

---
+++ ClassLibrary1/StringCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            if (number.Length == 0) return 0;
+            return int.Parse(number);
+        }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,30 @@
+using ClassLibrary1;
+
 namespace TestProject1
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly StringCalculator _calculator;
 
+        public UnitTest1()
+        {
+            _calculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void ZeroTest()
+        {
+            Assert.Equal(0, _calculator.Add(""));
+        }
+
+        [Theory]
+        [InlineData("2",2)]
+        [InlineData("3", 3)]
+        [InlineData("33", 33)]
+        [InlineData("4511", 4511)]
+        public void WhenStringProvided_ItShouldReturnItsValue(string input, int output)
+        {
+            Assert.Equal(output,_calculator.Add(input));
         }
     }
 }
